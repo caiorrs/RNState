3c315a6262dbeb3650b071372a9483762caf6c7c
Caio Roberto <caiorrs@gmail.com>
2022-02-02 17:06:52 -0300

    Managing state with zustand - no folder structure

--- App.tsx
+++ App.tsx
@@ -6,33 +6,75 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import {Pokemon, PokemonResponse} from './types';
 import React, {useEffect, useState} from 'react';
 
-import {Pokemon} from './types';
+import {AxiosResponse} from 'axios';
+import create from 'zustand';
 import {getPokemonList} from './service';
+import shallow from 'zustand/shallow';
 
-const App = () => {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-  const [loading, setLoading] = useState(true);
+type CountState = {
+  count: number;
+  increment: () => void;
+  reset: () => void;
+};
 
-  const [count, setCount] = useState(0);
+type PokemonState = {
+  pokemon: Pokemon[];
+  fetchPokemonList: () => void;
+  loading: boolean;
+  error: boolean;
+};
 
-  useEffect(() => {
-    void fetchPokemon();
-  }, []);
+const countStore = create<CountState>(set => ({
+  count: 0,
+  increment: () => set(state => ({count: state.count + 1})),
+  reset: () => set(() => ({count: 0})),
+}));
 
-  const fetchPokemon = async () => {
-    setLoading(true);
+const pokemonStore = create<PokemonState>(set => ({
+  pokemon: [],
+  loading: false,
+  error: false,
+  fetchPokemonList: async () => {
+    set({loading: true});
     try {
       const response = await getPokemonList();
-      setPokemon(response.data.results);
+      set({pokemon: response.data.results});
+      set({error: false});
     } catch (error) {
-      console.warn({error});
-      console.log('Error getting Pokemon list');
+      set({error: true});
     } finally {
-      setLoading(false);
+      set({loading: false});
     }
-  };
+  },
+}));
+
+const App = () => {
+  const {pokemon, loading, fetchPokemonList} = pokemonStore(
+    state => ({
+      pokemon: state.pokemon,
+      loading: state.loading,
+      error: state.error,
+      fetchPokemonList: state.fetchPokemonList,
+    }),
+    shallow,
+  );
+
+  // const {count, increment, reset} = countStore();
+  const {count, increment, reset} = countStore(
+    state => ({
+      count: state.count,
+      increment: state.increment,
+      reset: state.reset,
+    }),
+    shallow,
+  );
+
+  useEffect(() => {
+    void fetchPokemonList();
+  }, []);
 
   const renderItem = ({item}: {item: Pokemon}) => {
     return (
@@ -72,13 +114,18 @@ const App = () => {
           paddingVertical: 10,
         }}>
         <Text style={{fontSize: 30}}>{`Count: ${count}`}</Text>
-        <TouchableOpacity onPress={() => setCount(prev => prev + 1)}>
+        <TouchableOpacity onPress={increment}>
           <Text style={{fontSize: 30}}>+</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => setCount(0)}>
+        <TouchableOpacity onPress={() => countStore.setState({count: 0})}>
           <Text style={{fontSize: 30}}>RESET</Text>
         </TouchableOpacity>
       </View>
+      <TouchableOpacity
+        onPress={fetchPokemonList}
+        style={{backgroundColor: 'lightblue', paddingVertical: 10}}>
+        <Text style={{fontSize: 30, textAlign: 'center'}}>Fetch Again</Text>
+      </TouchableOpacity>
     </View>
   );
 };
